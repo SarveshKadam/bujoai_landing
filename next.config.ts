1aa83b241826b8ed99fe080d697aa9d761f61f47
SarveshKadam <kadamsarvesh1905@gmail.com>
2026-03-31 22:06:01 +0530

    deployment changes

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
+const repoName = "bujoai_landing";
+
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
